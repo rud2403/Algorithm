2b433ed21ca98098410ace26c3102ad988b09726
BGM <48942326+rud2403@users.noreply.github.com>
2023-04-11 22:59:23 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 12784 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String temp;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		temp = sc.next();
+		
+		System.out.println(temp +"??!");
+		
+		
+	}
+
+}
