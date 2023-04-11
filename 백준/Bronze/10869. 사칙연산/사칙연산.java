9818277cc9833c85f77f94754545e416b21dd14a
BGM <48942326+rud2403@users.noreply.github.com>
2023-04-11 22:59:13 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 12860 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		int a = x.nextInt();
+		int b = x.nextInt();
+		x.close();
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+
+	}
+
+}
