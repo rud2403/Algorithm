25f6ea80a32fb2acb2b92a5619364b06e6e21984
BGM <48942326+rud2403@users.noreply.github.com>
2023-04-26 14:25:10 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -4,41 +4,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int mount = Integer.parseInt(br.readLine());
-        int five = 0;
+        int n = Integer.parseInt(br.readLine());
+
+        int five = n / 5;
         int three = 0;
-        boolean flag = false;
 
-        while (mount > 0) {
-            if (flag == false) {
-                if (mount >= 5) {
-                    mount -= 5;
-                    five++;
-                } else if (mount >= 3) {
-                    mount -= 3;
-                    three++;
-                } else if(five != 0) {
-                    flag = true;
-                } else {
-                    System.out.println(-1);
-                    return;
+        if (n % 5 != 0) {
+            while (five >= 0) {
+                int remainder = n - five * 5;
+                if (remainder % 3 == 0) {
+                    three = remainder / 3;
+                    break;
                 }
-
-            } else {
-                if(mount >= 3) {
-                    three++;
-                    mount -= 3;
-                } else if (five != 0){
-                    five --;
-                    mount += 5;
-                } else {
-                    System.out.println(-1);
-                    return;
-                }
+                five--;
             }
+        } else {
+            three = 0;
         }
 
-        System.out.println(five + three);
-
+        if (five < 0) {
+            System.out.println("-1");
+        } else {
+            System.out.println(five + three);
+        }
     }
 }
