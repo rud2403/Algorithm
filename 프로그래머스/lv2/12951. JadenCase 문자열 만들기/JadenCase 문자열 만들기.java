4aaced988b75c422e3f25789e08c84b739824969
BGM <48942326+rud2403@users.noreply.github.com>
2023-06-11 17:27:05 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,24 +1,15 @@
 class Solution {
     public String solution(String s) {
+        String answer = "";
         
-        boolean tf = false; // 이전 문자가 공백인지 판별
-        StringBuilder sb = new StringBuilder();
+        s.toUpperCase();
         
-        sb.append(Character.toUpperCase(s.charAt(0)));
-        for(int i = 1; i < s.length(); i++) {
-            if(s.charAt(i) == ' ') { // 문자가 띄어쓰기일 때
-                sb.append(s.charAt(i));
-                tf = true;
-            } else { // 문자가 일반 문자 일 때
-                if(tf) { // 이전 문자가 띄어쓰기였을 때
-                    sb.append(Character.toUpperCase(s.charAt(i)));
-                    tf = false;
-                } else { // 이전 문자가 띄어쓰기가 아니었을 때
-                    sb.append(Character.toLowerCase(s.charAt(i)));
-                }
-            }
-        }
+        System.out.print(s);
         
-        return sb.toString();
+//         for(int i = 0; i < s.length(); i++){
+            
+//         }
+        
+        return answer;
     }
 }
