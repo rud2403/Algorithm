8c8068d2f946f57414009518fa82b2c5590f81d6
BGM <48942326+rud2403@users.noreply.github.com>
2024-02-01 16:44:35 +0900

    [level 1] Title: 공원 산책, Time: 9.61 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -0,0 +1,91 @@
+class Solution {
+    public int[] solution(String[] park, String[] routes) {
+        int[] answer = {};
+        int[] cur = new int[2];
+        
+        // 스타트 위치 잡기
+        for(int i = 0; i < park.length; i++) {
+            for(int j = 0; j < park[i].length(); j++) {
+                if(park[i].charAt(j) == 'S') {
+                    cur[0] = i;
+                    cur[1] = j;
+                }
+            }
+        }
+        
+        for(int i = 0; i < routes.length; i++) {
+            String act = routes[i].split(" ")[0];
+            int num = Integer.parseInt(routes[i].split(" ")[1]);
+            int[] tempCur = new int[2];
+            tempCur[0] = cur[0];
+            tempCur[1] = cur[1];
+            
+            System.out.println("i : " + i);
+            System.out.println("시작 위치 : " + tempCur[0] + ", " + tempCur[1]);
+            
+            for(int j = 0; j < num; j++) {
+               if("E".equals(act)) {
+                   if((park[tempCur[0]].length() - 1) >= (tempCur[1] + 1)) {
+                     if(park[tempCur[0]].charAt(tempCur[1] + 1) == 'X') { // 장애물 만남
+                         tempCur = cur;
+                         break;
+                     } else { // 진행
+                         System.out.println("----------------------------------------------");
+                         tempCur[1]++;
+                     }
+                     
+                   } else { // 맵 밖으로 튀어 나감
+                       tempCur = cur;
+                       break;
+                   }
+                   
+               } else if("W".equals(act)) {
+                   if(0 <= (tempCur[1] - 1)) {
+                     if(park[tempCur[0]].charAt(tempCur[1] - 1) == 'X') { // 장애물 만남
+                         tempCur = cur;
+                         break;
+                     } else { // 진행
+                         tempCur[1]--;
+                     }
+                     
+                   } else { // 맵 밖으로 튀어 나감
+                       tempCur = cur;
+                       break;
+                   }
+               } else if("S".equals(act)) {
+                   if((park.length - 1) >= (tempCur[0] + 1)) {
+                     if(park[tempCur[0] + 1].charAt(tempCur[1]) == 'X') { // 장애물 만남
+                         tempCur = cur;
+                         break;
+                     } else { // 진행
+                         tempCur[0]++;
+                     }
+                     
+                   } else { // 맵 밖으로 튀어 나감
+                       tempCur = cur;
+                       break;
+                   }
+               } else {
+                   if(0 <= (tempCur[0] - 1)) {
+                     if(park[tempCur[0] - 1].charAt(tempCur[1]) == 'X') { // 장애물 만남
+                         tempCur = cur;
+                         break;
+                     } else { // 진행
+                         tempCur[0]--;
+                     }
+                     
+                   } else { // 맵 밖으로 튀어 나감
+                       tempCur = cur;
+                       break;
+                   }
+               }
+            }
+            
+            cur = tempCur;
+            
+        }
+        
+        
+        return cur;
+    }
+}
