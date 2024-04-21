19008e67770dc97ce79bbd341408f6230f4e5ac7
BGM <48942326+rud2403@users.noreply.github.com>
2024-04-21 15:24:28 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -2,14 +2,12 @@ class Solution {
     public int solution(int chicken) {
         int answer = 0;
         int coupon = chicken;
-        int bonusCk = 0;
         
         while(coupon >= 10) {
-            bonusCk = coupon / 10;
+            chicken = coupon / 10;
             coupon %= 10;
-            coupon += bonusCk;
-            answer += bonusCk;
-            bonusCk = 0;
+            coupon += chicken;
+            answer += chicken;
         }
         
         return answer;
