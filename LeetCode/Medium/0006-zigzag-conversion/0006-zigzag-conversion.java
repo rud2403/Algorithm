6438cbcaecc2fcb3d59ce5770b95e9058f927c76
BGM <48942326+rud2403@users.noreply.github.com>
2025-07-05 19:59:27 +0900

    Time: 5 ms (59.2%), Space: 44.6 MB (97.43%) - LeetHub

--- LeetCode/Medium/0006-zigzag-conversion/0006-zigzag-conversion.java
+++ LeetCode/Medium/0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,31 +1,29 @@
 class Solution {
     public String convert(String s, int numRows) {
-        String[][] strArr =  new String[1001][numRows];
-        int x = 0;
-        int y = 0;
-        boolean check = true;
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++) {
-            // System.out.println("x: " + x + " y: " + y);
-            // System.out.println("----------------------------");
-            strArr[x][y] = String.valueOf(s.charAt(i));
-            if((x == numRows-1 || x == 0) && i > 0) check = !check;
-            if(check) {
-                x += 1;
-            } else {
-                y += 1;
-                x -= 1;
-            }
+        if (numRows == 1) return s;
+
+        StringBuilder[] rows = new StringBuilder[Math.min(numRows, s.length())];
+        for (int i = 0; i < rows.length; i++) {
+            rows[i] = new StringBuilder();
         }
 
-        for(int i = 0; i < 1001; i++) {
-            for(int j = 0; j < 1001; j++) {
-                if(strArr[i][j] != null) {
-                    sb.append(strArr[i][j]);
-                }
-            }
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows[curRow].append(c);
+
+            // 위아래로 방향 바꾸기
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+
+            curRow += goingDown ? 1 : -1;
         }
 
-        return sb.toString();
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
     }
 }
