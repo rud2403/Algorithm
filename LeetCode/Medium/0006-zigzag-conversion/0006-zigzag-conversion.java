05b57e5ddb82c5a23fbe67284085a556ff70f92d
BGM <48942326+rud2403@users.noreply.github.com>
2025-07-05 20:01:42 +0900

    Time: 4 ms (83.61%), Space: 44.9 MB (78.78%) - LeetHub

--- LeetCode/Medium/0006-zigzag-conversion/0006-zigzag-conversion.java
+++ LeetCode/Medium/0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,29 +1,27 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1) return s;
-
+        if(numRows == 1) return s;
         StringBuilder[] rows = new StringBuilder[Math.min(numRows, s.length())];
-        for (int i = 0; i < rows.length; i++) {
+        for(int i = 0; i < rows.length; i++) {
             rows[i] = new StringBuilder();
         }
 
         int curRow = 0;
         boolean goingDown = false;
 
-        for (char c : s.toCharArray()) {
+        for(char c : s.toCharArray()) {
             rows[curRow].append(c);
 
-            // 위아래로 방향 바꾸기
-            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            // 위 아래로 방향 바꾸기
+            if(curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
 
             curRow += goingDown ? 1 : -1;
         }
 
         StringBuilder result = new StringBuilder();
-        for (StringBuilder row : rows) {
+        for(StringBuilder row : rows) {
             result.append(row);
         }
-
         return result.toString();
     }
 }
