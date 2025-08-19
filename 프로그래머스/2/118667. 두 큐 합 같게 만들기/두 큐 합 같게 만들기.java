e5a6e54a62fd911ab603ab9e108b8d524d3ee9b7
BGM <48942326+rud2403@users.noreply.github.com>
2025-08-20 00:18:01 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 64.40 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+// 어떻게 안되는 걸 판별할 수 있을지?
+// 준 쪽에서 한개만 남으면 안되는 듯
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = 0;
+        long sum1 = 0;
+        long sum2 = 0;
+        long totalSum = 0;
+        
+        
+        Deque<Integer> deque1 = new ArrayDeque<>();
+        Deque<Integer> deque2 = new ArrayDeque<>();
+        
+        for(int i : queue1) {
+            deque1.offerLast(i);
+            sum1 += i;
+        }
+        for(int i : queue2) {
+            deque2.offerLast(i);
+            sum2 += i;
+        }
+        
+        totalSum = sum1 + sum2;
+        
+        if(totalSum % 2 != 0) return -1;
+        
+        for(int i = 0; i < queue1.length * 3; i++) {
+            if(sum1 == sum2) return answer;
+            
+            if(sum1 > sum2) {
+                int n = deque1.pollFirst();
+                deque2.offerLast(n);
+                
+                sum1 -= n;
+                sum2 += n;
+
+            } else if(sum1 < sum2) {
+                int n = deque2.pollFirst();
+                deque1.offerLast(n);
+                
+                sum1 += n;
+                sum2 -= n;
+            }
+            answer++;
+        }
+        
+        return -1;
+    }
+}
